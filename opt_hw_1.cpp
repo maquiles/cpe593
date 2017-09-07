8e0a9efa7e0cec04b1aceb879de09297f2f020e6
maquiles <mattaquiles@gmail.com>
2017-09-07 11:51:43 -0400

    hw1

--- opt_hw_1.cpp
+++ opt_hw_1.cpp
@@ -3,30 +3,27 @@
 
 using namespace std;
 
-struct Memoize{
-    int memo[53][53] = {0};
-}
+int memo[53][53] = {0};
+//so this wont work if it si greater than 52
 
 int choose(int n, int r)
 {
-    int Memoize.memo[n+1][r+1];
-
     if (memo[n][r] != 0){
-        return Memoize.memo[n][r];
+        return memo[n][r];
     }
     
     if (r==0 || r==n)
     {
-        return Memoize.memo[n][r] = 1;
+        return memo[n][r] = 1;
     }
     else
     {
-        return Memoize.memo[n][r] = choose(n-1, r-1) + choose(n-1, r);
+        return memo[n][r] = choose(n-1, r-1) + choose(n-1, r);
     }
 }
 
 int main()
 {
-    cout<< choose(10,2) << endl;
+    cout<< choose(52,6) << endl;
     return 0;
 }
