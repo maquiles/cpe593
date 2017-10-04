dd3cf1c656437af1f2bebfa4774c689ce87c3cdf
maquiles <mattaquiles@gmail.com>
2017-10-03 20:31:56 -0400

    java string trick class notes

---
+++ class_notes/StringsInJava.java
@@ -0,0 +1,23 @@
+//psudo java code for string trick in java
+
+public class StringsInJava {
+    f(StringBuilder b){
+        b.append(...);
+    }
+    String f(Strin a, String b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        String s = "abc" + "def"; //abcdef
+        String s1 = "";
+        final int n = 1000000;
+        for (int i = 0; i < n; i++){
+            s1 += i; //s1 = "01234567891011121314...99"
+        }
+        for (int i = 0; i < n; i++){}
+            StringBuilder b = new StringBuilder();
+            b.append(i);
+        }
+        String s1 = b.toString();
+    }
+}
