ed4777b065afb8a356152181fe6b227c846d1690
maquiles <mattaquiles@gmail.com>
2017-10-21 18:30:08 -0400

    fixed GrowArray to add used variable like instructions stated

--- GrowArray.cpp
+++ GrowArray.cpp
@@ -17,12 +17,14 @@ using namespace std;
 class GrowArray {
 private:
     int capacity;
+    int used;
     int *data;
 
 public:
     GrowArray(){
         data = nullptr;
         capacity = 0;
+        used = 0;
     }
 
     ~GrowArray(){
@@ -31,6 +33,7 @@ public:
 
     GrowArray(const GrowArray& orig){
         capacity = orig.capacity;
+        used = orig.used;
         data = new int[capacity];
         for (int i = 0; i < capacity; i++){
             data[i] = orig.data[i];
@@ -41,6 +44,7 @@ public:
         if(this != &orig){
             delete[] data;
             capacity = orig.capacity;
+            used = orig.used;
             data = new int[capacity];
             for (int i = 0; i < capacity; i++){
                 data[i] = orig.data[i];
@@ -51,12 +55,21 @@ public:
 
     //add a number to the front of the GrowArray
     void addFirst(int a){
-        int* temp = new int[capacity+1];
+        if(used == capacity){
+            if(capacity == 0){
+                capacity = 1;
+            }
+            else{
+                capacity *= 2;
+            }
+        }
+
+        int* temp = new int[capacity];
         temp[0] = a;
-        for (int i = 0; i < capacity; i++){
+        for (int i = 0; i < used; i++){
             temp[i+1] = data[i];
         }
-        capacity++;
+        used++;
         delete[] data;
         data = temp;
     }
@@ -77,12 +90,21 @@ public:
 
     //add a number to the end of the GrowArray
     void addLast(int b){
-        int* temp = new int[capacity+1];
-        for (int i = 0; i < capacity; i++){
+        if(used == capacity){
+            if(capacity == 0){
+                capacity = 1;
+            }
+            else{
+                capacity *= 2;
+            }
+        }
+
+        int* temp = new int[capacity];
+        for (int i = 0; i < used; i++){
             temp[i] = data[i];
         }
-        temp[capacity] = b;
-        capacity++;
+        temp[used] = b;
+        used++;
         delete[] data;
         data = temp;
     }
@@ -104,47 +126,42 @@ public:
     //REMOVE_FRONT
     //means remove the first n numbers from the list
     void removeFront(int n){
-        if (capacity == 0){
+        if (capacity == 0 || used == 0){
             return;
         }
-        for (int i = 0; i < capacity-n; i++){
-            data[i] = data[i+n];
+
+        int* temp = new int[capacity];
+
+        for (int i = 0; i < used-n; i++){
+            temp[i] = data[i+n];
         }
-        capacity -= n;
+        delete[] data;
+        data = temp;
+        used -= n;
     }
 
     //REMOVE_BACK
     //means remove the last m numbers from the list
     void removeBack(int m){
-        if (capacity == 0){
+        if (capacity == 0 || used == 0){
             return;
         }
-        while(m != 0){
-            capacity--;
-            m--;
+
+        int* temp = new int[capacity];
+        for (int i = 0; i < used-m; i++){
+            temp[i] = data[i];
         }
+        delete[] data;
+        data = temp;
+        used -= m;
     }
 
     //OUTPUT
     void output(){
-        for (int i = 0; i < capacity; i++){
+        for (int i = 0; i < used; i++){
             cout<< data[i]<< ",";
         }
         cout<< "\n";
-    }
-
-    void clear(){
-        delete[] data;
-        data = nullptr;
-        capacity = 0;
-    }
-
-    int get(int i){
-        return data[i];
-    }
-
-    void set(int i, int val) {
-        data[i] = val;
     }
 
     int operator[] (int i) const {
