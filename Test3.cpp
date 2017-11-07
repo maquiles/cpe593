8753734a1462515242b05dc21af02b0400945ee8
Matthew Aquiles <mattaquiles@gmail.com>
2017-11-06 21:24:49 -0500

    studying more

--- Test3.cpp
+++ Test3.cpp
@@ -32,7 +32,8 @@
  * for nxn matrix A and column vector x
  * Ax is O(n^2)
  * 
- * LU factorization. should be able to demonstrate a single row of the algorithm (simple numbers, no calculator required)
+ * LU factorization - O(n^3)
+ * should be able to demonstrate a single row of the algorithm (simple numbers, no calculator required)
  * Ex: 
  * 2 1 3 1 0 0
  * 1 3 1 0 1 0
@@ -60,7 +61,7 @@ double getTriangleMatrix(int i, int j){
 }
 
 double get(int i, int j){ //O(1)
-    if(Math.abs(i-1) > 1){
+    if(Math.abs(i-1) > 1){ //this is a java thing i beleive
         return 0;
     }
     return m[3*i + j-i + 1]; // return m[2*i + j];
