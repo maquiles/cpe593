ed123f63a832b8f482ef4b0f98371c6b282d9428
Matthew Aquiles <mattaquiles@gmail.com>
2017-11-06 21:15:50 -0500

    studying

--- Test3.cpp
+++ Test3.cpp
@@ -5,17 +5,28 @@
  *** Stacks
  *** Queues
  * 
- * Matrix
- * representation and mapping rules for regular matrix
- * m[r*cols + c] triangular
+ * Matrix - complexity O(nmp) if n=m=p O(n^3)
+ * row major: m[i*cols + j]
+ * column major: m[j*rows +i]
+ * 
+ * lower triangular - O(n^2)
+ * upper triangular - O(n^2)
+ * 
  * diagonal
  * if(i != j) return 0;
  * return m[i]
  * 
- * tridiagonal: m[3*i + (j-i) + 1] --> m[i+i + j]
+ * tridiagonal: O(n)
+ * m[3*i + (j-i) + 1] --> m[i+i + j +1] --> [i*2 +j]
  * 
  * know definition and complexity for:
- * addition, subtraction, multiplication, gauss-jordan, partial pivoting, full-pivoting
+ * addition
+ * subtraction
+ * multiplication
+ * gauss-jordan
+ * partial pivoting
+ * 
+ * full-pivoting
  * be able to show partial or full pivoting on a matrix (just 1, not the whole thing)
  * be able to compute complexity
  * for nxn matrix A and column vector x
@@ -26,7 +37,7 @@
  * 2 1 3 1 0 0
  * 1 3 1 0 1 0
  * 
- * to wupe out the number below the 2 in the first column, multiply first row by -1/2
+ * to wipe out the number below the 2 in the first column, multiply first row by -1/2
  * 2 1     3   1 0 0
  * 1 2.5 -.5 -.5 1 0
  * 
@@ -40,4 +51,17 @@
  * 
 **/
 
+//triangular matrix psudo code
+double getTriangleMatrix(int i, int j){
+    if(j >i){
+        return 0;
+    }
+    return m[i*(i+1)/2+j];
+}
 
+double get(int i, int j){ //O(1)
+    if(Math.abs(i-1) > 1){
+        return 0;
+    }
+    return m[3*i + j-i + 1]; // return m[2*i + j];
+}
